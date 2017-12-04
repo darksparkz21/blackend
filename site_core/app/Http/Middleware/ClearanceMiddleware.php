73c136c01e508220d25dbd8e3da08597259c3c54
darksparkz21 <darksparkz21@users.noreply.github.com>
2017-12-05 05:57:33 +0800

    Clearance
    Middleware

---
+++ site_core/app/Http/Middleware/ClearanceMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class ClearanceMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->hasRole('SuperAdmin')){
+            return $next($request);
+        }
+
+        if($request->is('admin/*')){
+            if(!Auth::user()->hasRole('SuperAdmin')){
+                abort('401');
+            }
+            else{
+                return $next($request);
+            }
+        }
+    }
+}
