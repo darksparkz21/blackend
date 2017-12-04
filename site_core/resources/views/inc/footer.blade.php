e2fe9483d3560e00f769dfbc52009e4bc393ca37
darksparkz21 <darksparkz21@users.noreply.github.com>
2017-12-04 22:26:37 +0800

    views 2

---
+++ site_core/resources/views/inc/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+      <div class="container">
+        <span class="text-muted">About</span><br>
+        <span class="text-muted">Careers</span><br>
+        <span class="text-muted">Contact Us</span>
+      </div>
+</footer>
