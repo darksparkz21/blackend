4bd99f2677bc91ac47dc1938d2429e2a88acd3f2
darksparkz21 <darksparkz21@users.noreply.github.com>
2017-12-09 10:07:19 +0800

    local
    plugin files

--- site_core/resources/views/vendor/backpack/base/layout.blade.php
+++ site_core/resources/views/vendor/backpack/base/layout.blade.php
@@ -113,7 +113,8 @@
     @yield('before_scripts')
 
     <!-- jQuery 2.2.3 -->
-    <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+    {{--<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>--}}
+    <script src="{{ asset('js/jquery-2.2.3.min.js') }}"></script>
     <script>window.jQuery || document.write('<script src="{{ asset('vendor/adminlte') }}/plugins/jQuery/jQuery-2.2.3.min.js"><\/script>')</script>
     <!-- Bootstrap 3.3.5 -->
     <script src="{{ asset('vendor/adminlte') }}/bootstrap/js/bootstrap.min.js"></script>
